9ff87df0f6db64e6666386822982bd019a52f296
jesusjbr <jesusjavierbr@gmail.com>
2021-01-05 21:10:28 +0100

    Day 17 finished

--- src/day17.rs
+++ src/day17.rs
@@ -9,6 +9,7 @@ struct Cell {
     x: i32,
     y: i32,
     z: i32,
+    w: i32,
 }
 
 impl Cell {
@@ -16,7 +17,8 @@ impl Cell {
         (self.x - 1..=self.x + 1)
             .cartesian_product(self.y - 1..=self.y + 1)
             .cartesian_product(self.z - 1..=self.z + 1)
-            .map(|((x, y), z)| Cell { x, y, z })
+            .cartesian_product(self.w - 1..=self.w + 1)
+            .map(|(((x, y), z), w)| Cell { x, y, z, w })
             .filter(move |cell| cell != self) //A cell is not neighbour of itself.
     }
 }
@@ -26,6 +28,7 @@ struct World {
     borders_x: RangeInclusive<i32>,
     borders_y: RangeInclusive<i32>,
     borders_z: RangeInclusive<i32>,
+    borders_w: RangeInclusive<i32>,
 }
 
 impl Iterator for World {
@@ -34,7 +37,8 @@ impl Iterator for World {
         let new_active_cells: HashSet<Cell> = (self.borders_x.clone())
             .cartesian_product(self.borders_y.clone())
             .cartesian_product(self.borders_z.clone())
-            .map(|((x, y), z)| Cell { x, y, z })
+            .cartesian_product(self.borders_w.clone())
+            .map(|(((x, y), z), w)| Cell { x, y, z, w })
             .filter(|cell| {
                 let total = cell
                     .neighbours()
@@ -48,6 +52,7 @@ impl Iterator for World {
         self.borders_x = self.borders_x.start() - 1..=self.borders_x.end() + 1;
         self.borders_y = self.borders_y.start() - 1..=self.borders_y.end() + 1;
         self.borders_z = self.borders_z.start() - 1..=self.borders_z.end() + 1;
+        self.borders_w = self.borders_w.start() - 1..=self.borders_w.end() + 1;
         self.active_cells = new_active_cells;
         Some(self.active_cells.len())
     }
@@ -60,6 +65,7 @@ impl FromStr for World {
         let borders_y = -1..=s.lines().count() as i32;
         let borders_x = -1..=s.lines().last().unwrap().len() as i32;
         let borders_z = -1..=1;
+        let borders_w = -1..=1;
 
         let active_cells: HashSet<Cell> = s
             .lines()
@@ -71,6 +77,7 @@ impl FromStr for World {
                 x: j as i32,
                 y: i as i32,
                 z: 0,
+                w: 0,
             })
             .collect();
 
@@ -79,6 +86,7 @@ impl FromStr for World {
             borders_x,
             borders_y,
             borders_z,
+            borders_w,
         })
     }
 }
@@ -88,16 +96,16 @@ pub mod tests {
     use super::*;
 
     #[test]
-    fn example1_part1() {
+    fn example1_part2() {
         let input = include_str!("../inputs/day17_example1.txt");
         let world = World::from_str(input).unwrap();
-        assert_eq!(world.into_iter().nth(6 - 1).unwrap(), 112);
+        assert_eq!(world.into_iter().nth(6 - 1).unwrap(), 848);
     }
 
     #[test]
-    fn part1() {
+    fn part2() {
         let input = include_str!("../inputs/day17.txt");
         let world = World::from_str(input).unwrap();
-        assert_eq!(world.into_iter().nth(6 - 1).unwrap(), 280);
+        assert_eq!(world.into_iter().nth(6 - 1).unwrap(), 1696);
     }
 }
